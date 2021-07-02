a3abd4bbb1e258325c774a5fd5db991061e5084c
Evgenii Morgunov <morgunove87@gmail.com>
2021-07-02 13:41:05 +0500

    master

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-	"enable" => true,
-	"host" => "localhost",
-	"port" => 3307,
-	"user" => "root",
-	"password" => "",
-	"dbname" => "auth_php",
-];
--- index.php
+++ index.php
@@ -1,8 +1,3 @@
 <?php
-session_start ();
-use App\services\App;
 
-require_once __DIR__ . "/vendor/autoload.php";
-App::start ();
-require_once __DIR__ . "/router/routes.php";
 
