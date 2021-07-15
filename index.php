f33f159fbbb520fff1d460718d1dc7b8ad9b9f0f
MorgunovE <morgunove87@gmail.com>
2021-07-15 12:49:34 +0500

    Perevertish

--- index.php
+++ index.php
@@ -1,12 +1,73 @@
 <?php
-$url = "https://yandex.by";
+$arr_sites = [];
+//////////////////////////////////////////
+
+//function pr ( $n )
+//{
+//	echo '<pre>';
+//	print_r ( $n );
+//	echo '</pre>';
+//}
+//
+//function mb_strrev ( $str )
+//{
+//	$r = '';
+//	for ( $i = mb_strlen ( $str ) ; $i >= 0 ; $i-- ) {
+//		$r .= mb_substr ( $str, $i, 1 );
+//	}
+//	return $r;
+//}
+//
+//function isPoly ( $n )
+//{
+//	$result = 0;
+//	$n = trim ( $n );
+//	$symbols = [ ' ', '.', ':', ',', '%', '$', '#', '@', '!', '^', '&', '*' ];
+//	$n = mb_strtolower ( str_replace ( $symbols, '', $n ) );
+//	$revers = mb_strrev ( $n );
+//
+//	if ( $n == $revers ) {
+//		$result = 1;
+//	}
+////	pr ( $n );
+////	pr ( $revers );
+////	pr ( "------" );
+//	return $result;
+//}
+//
+//$letPhrases = [
+//	"Тут как тут",
+//
+//	"Коту тащат уток",
+//
+//	"15.01.2002 10:51",
+//
+//	"Я разуму уму заря",
+//
+//	"Искать такси",
+//
+//	"Дивен мне вид",
+//
+//	'Дивен вид',
+//
+//	'Vне вид',
+//];
+//foreach ( $letPhrases as $phrase ) {
+//	$result = 'Word is not palindrome';
+//	$res = isPoly ( $phrase );
+//	if ( $res ) $result = "Word is palindrome";
+//	pr ( $phrase . " - " . $result );
+//}
+//pr ( $letPhrases );
+//////////////////////////////////////////////
+//$url = "https://yandex.by";
 //$cont = file_get_contents($url);
 //print_r ( $cont);
 
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, $url);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-$res = curl_exec($curl);
+//$curl = curl_init();
+//curl_setopt($curl, CURLOPT_URL, $url);
+//curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+//$res = curl_exec($curl);
 //print_r ( $res);
 
 //preg_match('#<title>(.*?)</title>#su', $res, $matches);
